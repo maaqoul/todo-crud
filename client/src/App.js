34b85ab5ede01304f2af2ccee879f5be148bfd0d
splintos <mokhtar.elidrissi1987@gmail.com>
2022-09-22 06:12:17 +0100

    import react + edit button Add

--- client/src/App.js
+++ client/src/App.js
@@ -1,60 +1,71 @@
 import React, { useEffect, useState } from "react";
 import TodoElement from "./components/TodoElement";
+import Button from "react-bootstrap/Button";
 
 function App() {
   const url = "http://localhost:3001/api/todo";
 
-  
-  const [todo, setTodo] = useState('');
+  const [todo, setTodo] = useState("");
   const [todos, setTodos] = useState([]);
 
   useEffect(() => {
-    fetch(url).then(response => response.json()).then(todos => {
-      console.log("todos :", todos);
-      setTodos(todos)
-    }).catch(e => console.error(e))
-  }, [todo])
-  
-  const handleInputChange = (e) => {
+    fetch(url)
+      .then((response) => response.json())
+      .then((todos) => {
+        console.log("todos :", todos);
+        setTodos(todos);
+      })
+      .catch((e) => console.error(e));
+  }, [todo]);
 
-    setTodo(e.target.value)
+  const handleInputChange = (e) => {
+    setTodo(e.target.value);
   };
 
   const handleOnClick = () => {
-    if(todo) {
+    if (todo) {
       // ;
-      setTodo('')
-      fetch(url, {method: "POST", body: JSON.stringify({item: todo, done: false}), headers: { 'Content-Type': 'application/json' },}, ).then(response => {
-        return response.json()
-      }).then(r => {
-        setTodos([...todos, {item: todo, id: r ? r._id : null ,done: false}])
+      setTodo("");
+      fetch(url, {
+        method: "POST",
+        body: JSON.stringify({ item: todo, done: false }),
+        headers: { "Content-Type": "application/json" },
       })
+        .then((response) => {
+          return response.json();
+        })
+        .then((r) => {
+          setTodos([
+            ...todos,
+            { item: todo, id: r ? r._id : null, done: false },
+          ]);
+        });
     }
-  }
+  };
 
   const toggleDone = (id) => {
-    const todo = todos.find(todo => todo.id === id)
+    const todo = todos.find((todo) => todo.id === id);
     todo.done = !todo.done;
     setTodos([...todos]);
-  }
- 
+  };
+
   return (
     <div className="App">
-      <header>
-        Icon
-      </header>
+      <header>Icon</header>
       <main>
-        <input type="text" value={todo}  onChange={handleInputChange}/> | <button onClick={handleOnClick}>add</button>
-
+        <input type="text" value={todo} onChange={handleInputChange} /> |{" "}
+        <Button variant="primary" onClick={handleOnClick}>
+          Add
+        </Button>{" "}
         <div>
           <ul>
-            {todos.map((t) => (<TodoElement todo={t} key={t['_id']} toggle={toggleDone}/>))}
+            {todos.map((t) => (
+              <TodoElement todo={t} key={t["_id"]} toggle={toggleDone} />
+            ))}
           </ul>
         </div>
       </main>
-      <footer>
-        footer
-      </footer>
+      <footer>footer</footer>
     </div>
   );
 }
--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+/* 
+React boostrap configurations
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+*/
+import "../node_modules/react-bootstrap/dist/react-bootstrap";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-
